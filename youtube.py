797da1af93b38efb3d92411dff2edd5afdd4872f
vn-ki <appukuttancr@gmail.com>
2018-01-13 03:54:17 +0530

    Final

--- youtube.py
+++ youtube.py
@@ -1,25 +1,16 @@
-import youtube_channel
 from feed import feed, story
-from urllib import urlencode
+from urllib.parse import urlencode
 from urllib import request
+import json
+import re
 
 
 def get_feed(name):
-    channel = youtube_channel.get_channel(name)
-    uploads = channel.uploads
+    uploads = get_channel_uploads(name)
     youtube_videos = feed([])
-    i = 0
-    for x in uploads:
-        s = story(url=x.watchv_url,
-                  title=x.title,
-                  pub_time=x.published,
-                  content=x.description,
-                  source='youtube',
-                  ext_links=[])
+    for item in uploads['items']:
+        s = return_story(item)
         youtube_videos.append(story=s)
-        i += 1
-        if i == 50:
-            break
 
     return youtube_videos
 
@@ -29,9 +20,77 @@ def call_gdata(api, qs):
     qs['key'] = 'AIzaSyDvysm00R5FClmqtxcATsgpKHdt2GxCaiU'
     url = 'https://www.googleapis.com/youtube/v3/' + api + '?' + urlencode(qs)
 
-    try:
-        data = request.urlopen(url).read().decode('utf-8')
-    except HTTPError as e:
-        raise
+    data = request.urlopen(url).read().decode('utf-8')
 
     return json.loads(data)
+
+
+def get_channel_uploads(url):
+    query = None
+    chanR = re.compile('.+channel\/([^\/]+)$')
+    userR = re.compile('.+user\/([^\/]+)$')
+    channel_id = None
+    channel_url = url
+    if chanR.match(channel_url):
+        channel_id = chanR.search(channel_url).group(1)
+    elif userR.match(channel_url):
+        username = userR.search(channel_url).group(1)
+        query = {'part': 'snippet, contentDetails, statistics',
+                 'forUsername': username}
+    elif len(channel_url) == 24 and channel_url[:2] == 'UC':
+        channel_id = channel_url
+    else:
+        username = channel_url
+        query = {'part': 'snippet, contentDetails, statistics',
+                 'forUsername': username}
+
+    if query is None:
+        query = {'part': 'snippet, contentDetails, statistics',
+                 'id': channel_id}
+    allinfo = call_gdata('channels', query)
+
+    try:
+        ch = allinfo['items'][0]
+    except IndexError:
+        err = "Unrecognized channel id, url or name : %s"
+        raise ValueError(err % channel_url)
+
+    return get_uploads(ch['contentDetails']['relatedPlaylists']['uploads'])
+
+
+def get_uploads(upload_id):
+    query = {'part': 'snippet',
+             'maxResults': 50,
+             'playlistId': upload_id}
+
+    data = call_gdata('playlistItems', query)
+
+    return data
+
+
+def return_story(item):
+    s = story(url='www.youtube.com/watch?v='+item['snippet']['resourceId']['videoId'],
+              title=item['snippet']['title'],
+              pub_time=parseISO8591(item['snippet']['publishedAt']),
+              content=item['snippet']['description'],
+              source='youtube',
+              ext_links=[])
+
+    return s
+
+
+def parseISO8591(duration):
+    """ Parse ISO 8591 formated duration """
+    regex = re.compile(r'PT((\d{1,3})H)?((\d{1,3})M)?((\d{1,2})S)?')
+    if duration:
+        duration = regex.findall(duration)
+        if len(duration) > 0:
+            _, hours, _, minutes, _, seconds = duration[0]
+            duration = [seconds, minutes, hours]
+            duration = [int(v) if len(v) > 0 else 0 for v in duration]
+            duration = sum([60**p*v for p, v in enumerate(duration)])
+        else:
+            duration = 30
+    else:
+        duration = 30
+    return duration
