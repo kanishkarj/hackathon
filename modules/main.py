7363877b420a1e9453f9ccbdf8842ec93c17eb97
vn-ki <appukuttancr@gmail.com>
2018-01-13 04:20:54 +0530

    Fix some stuff

--- modules/main.py
+++ modules/main.py
@@ -1,6 +1,6 @@
 from importlib import import_module
 
 def get_feed(sm, name):
-    social_media = import_module(sm)
+    social_media = import_module('.{0}'.format(sm), 'sources')
     feed = social_media.get_feed(name)
     return feed
