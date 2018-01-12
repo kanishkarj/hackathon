22a8ec041826c08f61402273880786adcdc30186
kanishkarj <kanishkarj@hotmail.com>
2018-01-13 03:57:59 +0530

    temp

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 from bottle import route, run, template
+from modules.sources.facebook import scrapeFacebookPageFeedStatus
+
+app_id = "542979522719768"
+app_secret = "c1dfb166b76b79bd19f3ca811bea1aba"  # DO NOT SHARE WITH ANYONE!
+page_id = "pclubiiti"
+
+# input date formatted as YYYY-MM-DD
+since_date = ""
+until_date = ""
+
+access_token = app_id + "|" + app_secret
 
 @route('/')
 def hello():
+    res = scrapeFacebookPageFeedStatus(page_id,access_token,since_date,until_date)
     return template('home')
 
 
