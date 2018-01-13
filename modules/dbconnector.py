42015ad914fb86dde4868d98b6a9d0868c6bc216
kanishkarj <kanishkarj@hotmail.com>
2018-01-13 12:38:16 +0530

    Merge

--- modules/dbconnector.py
+++ modules/dbconnector.py
@@ -2,17 +2,30 @@ from pymongo import MongoClient
 from pymongo.collation import Collation
 import datetime
 from modules import main
+from gi.repository import GLib
+import json;
+import pyrebase
+
 
 class dbconnector :
 
     def __init__(self):
-        self.client = MongoClient('localhost', 27017)
-        self.db = self.client.hackathon
-        self.coll = self.db['table']
+        self.conf = {
+          'apiKey': "AIzaSyB5ZW4igirOCICYO_lYxiTRbYJ61R-OVuo",
+          'authDomain': "epic-shit.firebaseapp.com",
+          'databaseURL': "https://epic-shit.firebaseio.com",
+          'projectId': "epic-shit",
+          'storageBucket': "epic-shit.appspot.com",
+          'messagingSenderId': "135240258150"
+        }
+        self.config = json.dumps(self.conf)
+        self.firebase = pyrebase.initialize_app(self.conf)
+        self.db = self.firebase.database()
+        self.data = self.db.child("data")
 
     def db_insert(self,source,title) :
         res = main.get_feed(source,title);
-        self.coll.insert_one({
+        self.data.push({
             'source':source,
             'title':title,
             'res':res.toJson(),
@@ -22,10 +35,8 @@ class dbconnector :
 
     def db_get(self,source,title):
         result = []
-        res = self.coll.find({
-            'source':source,
-            'title':title
-        })
+        res = self.data.order_by_child("source").equal_to(source).order_by_child('title').equal_to(title)
+
         if len(result) == 0:
             result = self.db_insert(source,title).toJson()
         else :
@@ -33,13 +44,17 @@ class dbconnector :
                 result.append(x)
         return result
 
+
     def update_db(self) :
-        res = self.coll.find();
+        res = self.data.get();
         for x in res:
             y = main.get_feed(x['source'],x['title'])
-            self.coll.update_one({
+            self.data.update({
                 'source':x['source'],
-                'title':x['title'],
                 'res':y.toJson(),
+                'title':x['title'],
                 'time': str(datetime.time)
             })
+
+    def initiate_update(self) :
+        GLib.timeout_add_seconds(1, self.update_db)
