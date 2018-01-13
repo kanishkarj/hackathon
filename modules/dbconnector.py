c1f79f3b78e47d70d4720f505a8d93280f19400a
kanishkarj <kanishkarj@hotmail.com>
2018-01-13 10:18:48 +0530

    Post API endpoint

--- modules/dbconnector.py
+++ modules/dbconnector.py
@@ -10,15 +10,28 @@ class dbconnector :
         self.db = self.client.hackathon
         self.coll = self.db['table']
 
-    def db_insert(self,source,title,res) :
+    def db_insert(self,source,title) :
+        res = main.get_feed(source,title);
         self.coll.insert_one({
             'source':source,
             'title':title,
             'res':res.toJson(),
             'time': str(datetime.time)
         })
+        return res
 
-
+    def db_get(self,source,title):
+        result = []
+        res = self.coll.find({
+            'source':source,
+            'title':title
+        })
+        if len(result) == 0:
+            result = self.db_insert(source,title).toJson()
+        else :
+            for x in res:
+                result.append(x)
+        return result
 
     def update_db(self) :
         res = self.coll.find();
