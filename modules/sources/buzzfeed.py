d2760113d68a22e4e0a49e291398a2fd8d231e51
TarunKS17 <tarunkumarsingh98@gmail.com>
2018-01-13 10:54:43 +0530

    Buzzfeed

---
+++ modules/sources/buzzfeed.py
@@ -0,0 +1,102 @@
+#!python3
+import json
+import requests
+from ..feed import feed, story
+
+
+def get_feed(name):
+    r=requests.get('https://www.buzzfeed.com/buzzfeed/search2_json?q='+name)
+    data=json.loads(r.text)
+    final_feed = feed([])
+    for t in data['response']['results']:
+        new_story = story(title=t['name'], pub_time= t['published_unix'], content= t['blurb'], url= t['link'],
+            source='Buzzfeed', ext_links=t['m_image'])
+        final_feed.append(new_story)
+    return final_feed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+getcontent('football')
